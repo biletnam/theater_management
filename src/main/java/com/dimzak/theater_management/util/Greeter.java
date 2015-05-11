afd492fc007adffe34435700a031bcd91c2b9456
dimzak <jim.thomas.zks@gmail.com>
2015-05-11 23:29:47 +0300

    dummy class for testing arquillian

---
+++ src/main/java/com/dimzak/theater_management/util/Greeter.java
@@ -0,0 +1,18 @@
+package com.dimzak.theater_management.util;
+
+
+import java.io.PrintStream;
+
+/**
+ * A component for creating personal greetings.
+ */
+public class Greeter {
+
+    public void greet(PrintStream to, String name) {
+        to.println(createGreeting(name));
+    }
+
+    public String createGreeting(String name) {
+        return "Hello, " + name + "!";
+    }
+}
